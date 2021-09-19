68b1d0e9ea6fb6f70566db80e60225cf154bb428
Rishi Kothari <itsrishikothari@gmail.com>
2021-09-18 23:38:47 -0500

    gql commits

---
+++ src/shared/graphql/index.ts
@@ -0,0 +1,90 @@
+import { Type } from '@nestjs/common'
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql'
+import { BaseEntityService, IBaseEntityService } from '../database'
+import { plural } from 'pluralize'
+import { mapPaginationToFindProps, PaginationInput } from './Pagination'
+
+export const BaseEntityResolver = <Entity extends Type<unknown>>(
+	associatedEntityReference: Entity,
+	removal = false
+): any => {
+	@Resolver({ isAbstract: true })
+	abstract class AbstractBase {
+		constructor(private service: IBaseEntityService<Entity, any>) {}
+
+		@Query(() => [associatedEntityReference], {
+			name: plural(associatedEntityReference.name).toLowerCase(),
+		})
+		async findMany(
+			@Args('options', { nullable: true }) options: PaginationInput
+		): Promise<Entity[]> {
+			return this.service.findMany({
+				...mapPaginationToFindProps(options),
+			})
+		}
+
+		@Query(() => associatedEntityReference, {
+			name: associatedEntityReference.name.toLowerCase(),
+		})
+		async find(@Args('id') id: string): Promise<Entity> {
+			return this.service.find(id)
+		}
+
+		@Query(() => [associatedEntityReference], {
+			name: `bulk${plural(associatedEntityReference.name)}`,
+		})
+		async findByIDs(
+			@Args('ids', { type: () => [String] }) ids: string[]
+		): Promise<Entity[]> {
+			return this.service.findByIDs(ids)
+		}
+
+		@Mutation(() => Boolean, {
+			name: `remove${associatedEntityReference.name}`,
+		})
+		async remove(@Args('id') id: string) {
+			return this.service.remove(id)
+		}
+
+		@Mutation(() => [associatedEntityReference], {
+			name: `remove${plural(associatedEntityReference.name)}`,
+		})
+		async removeMany(@Args('ids', { type: () => [String] }) ids: string[]) {
+			return this.service.removeMany(ids)
+		}
+	}
+
+	@Resolver({ isAbstract: true })
+	abstract class AbstractBaseRemoved {
+		constructor(private service: IBaseEntityService<Entity, any>) {}
+
+		@Query(() => [associatedEntityReference], {
+			name: plural(associatedEntityReference.name).toLowerCase(),
+		})
+		async findMany(
+			@Args('options', { nullable: true }) options: PaginationInput
+		): Promise<Entity[]> {
+			return this.service.findMany({
+				...mapPaginationToFindProps(options),
+			})
+		}
+
+		@Query(() => associatedEntityReference, {
+			name: associatedEntityReference.name.toLowerCase(),
+		})
+		async find(@Args('id') id: string): Promise<Entity> {
+			return this.service.find(id)
+		}
+
+		@Query(() => [associatedEntityReference], {
+			name: `bulk${plural(associatedEntityReference.name)}`,
+		})
+		async findByIDs(
+			@Args('ids', { type: () => [String] }) ids: string[]
+		): Promise<Entity[]> {
+			return this.service.findByIDs(ids)
+		}
+	}
+
+	return removal ? AbstractBase : AbstractBaseRemoved
+}
